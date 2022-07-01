766e53ec23f3cf44f51c45d4bb4eb989dddca755
kelvin maritim <kelvin.maritim@strathmore.edu>
2022-07-01 21:46:41 +0300

    correct features

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function setupModeButtons(){
 			modeButtons[0].classList.remove("selected");
 			modeButtons[1].classList.remove("selected");
 			this.classList.add("selected");
-			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			this.textContent === "6" ? numSquares = 6: numSquares = 9;
 			reset();
 		});
 	}
